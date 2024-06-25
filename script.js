ac0de9f62d1f83cb9d132bd2b7c2b4133f633385
MaverickNub <you@example.com>
2024-06-26 01:12:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD47bB5rv0nnbBxDQ6ideeEpkjaHbyjzeo",
+    authDomain: "projectreview-ed3ec.firebaseapp.com",
+    databaseURL: "https://projectreview-ed3ec-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "projectreview-ed3ec",
+    storageBucket: "projectreview-ed3ec.appspot.com",
+    messagingSenderId: "800175800322",
+    appId: "1:800175800322:web:1063ea1b842e1254dbe71b"
+};
+
+const app = initializeApp(firebaseConfig)
+
